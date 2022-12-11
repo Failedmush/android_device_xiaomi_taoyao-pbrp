7d23747daac0ef779266a44ea129a0324c933d40
erwinabs <erwinabubakarsidik@gmail.com>
2022-12-11 06:59:31 +0000

    taoyao: PBRP

--- omni_taoyao.mk
+++ omni_taoyao.mk
@@ -19,13 +19,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from taoyao device
 $(call inherit-product, device/xiaomi/taoyao/device.mk)
 
 PRODUCT_DEVICE := taoyao
-PRODUCT_NAME := twrp_taoyao
+PRODUCT_NAME := omni_taoyao
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 12 Lite 5G
 PRODUCT_MANUFACTURER := xiaomi
